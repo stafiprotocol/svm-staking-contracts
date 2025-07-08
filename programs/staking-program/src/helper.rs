9db46ee61cbf51dcb37d296ea30aba4cb5cf38fa
tpkeeper <tpkeeper@SKY.local>
2025-07-08 11:55:51 +0800

    reduce default min stake amount

--- programs/staking-program/src/helper.rs
+++ programs/staking-program/src/helper.rs
@@ -2,4 +2,4 @@ pub const POOL_SEED: &'static [u8] = b"pool_seed";
 pub const STAKE_ACCOUNT_SEED: &'static [u8] = b"stake_account_seed";
 
 pub const REWARD_CALC_BASE: u128 = 1_000_000_000_000;
-pub const DEFAULT_MIN_STAKE_AMOUNT: u64 = 1_000_000_000;
+pub const DEFAULT_MIN_STAKE_AMOUNT: u64 = 1_000;
